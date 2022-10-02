f92a991fcd204222d3be6f5d912bfada563887eb
Louis Chatriot <louis.chatriot@gmail.com>
2022-10-02 18:59:12 +0200

    Right API

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import time
+
+
 N = 3
 
 def clone_grid(g):
@@ -67,9 +70,14 @@ def propagate(pos, i0, j0):
 
 
 # square between 0 and N ** 4 - 1
-def search(pos, square):
+def search(pos, square, sols):
     if square == N ** 4 - 1:
-        return pos   # If propagation was correct this is the solution
+        sols.append(pos)
+        return None
+        # return pos   # If propagation was correct this is the solution
+
+    if square >= N ** 4:
+        return None
 
     i = square // N ** 2
     j = square - i * N ** 2
@@ -78,26 +86,32 @@ def search(pos, square):
         return None   # Should not happen if propagation done well, except if input is buggy
 
     if len(pos[i][j]) == 1:
-        return search(pos, square + 1)
+        search(pos, square + 1, sols)
+        return None
 
     for c in pos[i][j]:
         _pos = clone_grid(pos)
         _pos[i][j] = c
 
         if propagate(_pos, i, j) is None:
-            return None
+            continue
+            # return None
 
-        res = search(_pos, square + 1)
-        if res is not None:
-            return res
+        res = search(_pos, square + 1, sols)
+        # if res is not None:
+        #    return res
 
     return None
 
 
 # Assumes it gets passed a sudoku grid with positive ints
-def check_validity(solved):
-    t = N ** 2 * (N ** 2 + 1) / 2
-
+def check_validity(solved, checksum):
+    for i in range(0, N ** 2):
+        for j in range(0, N ** 2):
+            if type(solved[i][j]) != int:
+                return False
+            if solved[i][j] < 0 or solved[i][j] > N ** 2:
+                return False
 
     for i in range(0, N ** 2):
         l1 = []
@@ -122,35 +136,58 @@ def check_validity(solved):
         l3 = sorted(l3)
 
         for z in range(1, N ** 2):
-            if l1[z] == l1[z - 1]:
+            if l1[z] != 0 and l1[z] == l1[z - 1]:
                 return False
 
-            if l2[z] == l2[z - 1]:
+            if l2[z] != 0 and l2[z] == l2[z - 1]:
                 return False
 
-            if l3[z] == l3[z - 1]:
+            if l3[z] != 0 and l3[z] == l3[z - 1]:
                 return False
 
-        if sum(l1) != t:
-            return False
+        if checksum:
+            t = N ** 2 * (N ** 2 + 1) / 2
 
-        if sum(l2) != t:
-            return False
+            if sum(l1) != t:
+                return False
 
-        if sum(l3) != t:
-            return False
+            if sum(l2) != t:
+                return False
+
+            if sum(l3) != t:
+                return False
+
+    return True
+
+
+def check_consistency(puzzle, solved):
+    for i in range(0, N ** 2):
+        for j in range(0, N ** 2):
+            if puzzle[i][j] != 0 and puzzle[i][j] != solved[i][j]:
+                return False
 
     return True
 
 
 def sudoku_solver(puzzle):
+    if check_validity(puzzle, False) is False:
+        raise RuntimeError("Invalid input")
+
     pos = create_possibilities(puzzle)
-    _res = search(pos, 0)
 
-    # Kata expects ints
-    res = list(map(lambda l: list(map(int, l)), _res))
+    sols = []
+    search(pos, 0, sols)
 
-    return res
+    if len(sols) == 1:
+        res = sols[0]
+        res = list(map(lambda l: list(map(int, l)), res))
+        return res
+    else:
+        raise RuntimeError("No or multiple solutions")
+
+    return None
+
+
 
 
 
@@ -169,64 +206,19 @@ puzzle = [
     [1, 0, 0, 0, 0, 2, 5, 0, 0]
 ]
 
-solution = [
-    [3, 4, 6, 1, 2, 7, 9, 5, 8], 
-    [7, 8, 5, 6, 9, 4, 1, 3, 2], 
-    [2, 1, 9, 3, 8, 5, 4, 6, 7], 
-    [4, 6, 2, 5, 3, 1, 8, 7, 9], 
-    [9, 3, 1, 2, 7, 8, 6, 4, 5], 
-    [8, 5, 7, 9, 4, 6, 2, 1, 3], 
-    [5, 9, 8, 4, 1, 3, 7, 2, 6],
-    [6, 2, 4, 7, 5, 9, 3, 8, 1],
-    [1, 7, 3, 8, 6, 2, 5, 9, 4]
-]
+p1 = [[0, 9, 6, 5, 0, 4, 0, 7, 1], [0, 2, 0, 1, 0, 0, 0, 0, 0], [0, 1, 4, 0, 9, 0, 6, 2, 3], [0, 0, 3, 0, 6, 0, 0, 8, 0], [0, 0, 8, 0, 5, 0, 4, 0, 0], [9, 0, 0, 4, 0, 0, 0, 0, 5], [7, 0, 0, 0, 0, 9, 0, 0, 0], [0, 0, 1, 0, 7, 5, 3, 4, 9], [2, 3, 0, 0, 4, 8, 1, 0, 7]]
+
+p10 = [[0, 0, 0, 0, 0, 2, 7, 5, 0], [0, 1, 8, 0, 9, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [4, 9, 0, 0, 0, 0, 0, 0, 0], [0, 3, 0, 0, 0, 0, 0, 0, 8], [0, 0, 0, 7, 0, 0, 2, 0, 0], [0, 0, 0, 0, 3, 0, 0, 0, 9], [7, 0, 0, 0, 0, 0, 0, 0, 0], [5, 0, 0, 0, 0, 0, 0, 8, 0]]
+
+print("===============================================================")
 
 print_grid(puzzle)
-pos = create_possibilities(puzzle)
 
-print_grid(pos)
+start = time.time()
 
-"""
-pos[3][5] = '6'
-propagate(pos, 3, 5)
+res = sudoku_solver(p10)
 
-print_grid(pos)
-"""
-
-
-res = search(pos, 0)
-
-print_grid(res)
-
-
-toptop = sudoku_solver(puzzle)
-
-
-print_grid(toptop)
-
-
-print(check_validity(toptop))
-
-print("===========================================")
-
-bogus = [
-    [4, 0, 0, 0, 0, 0, 0, 0, 0],
-    [7, 0, 8, 5, 0, 0, 0, 0, 4],
-    [0, 9, 0, 0, 0, 0, 3, 2, 0],
-    [3, 0, 0, 0, 6, 2, 0, 0, 8],
-    [0, 0, 0, 1, 0, 8, 0, 0, 0],
-    [2, 0, 0, 9, 5, 0, 0, 0, 6],
-    [0, 2, 7, 0, 0, 0, 0, 8, 0],
-    [6, 0, 0, 0, 0, 5, 9, 0, 7],
-    [0, 0, 0, 0, 0, 0, 0, 0, 2]
-]
-
-print_grid(bogus)
-
-bogres = sudoku_solver(bogus)
-
-print_grid(bogres)
-
-print(check_validity(bogres))
+duration = time.time() - start
+print(f"==> Duration: {duration}")
 
 
